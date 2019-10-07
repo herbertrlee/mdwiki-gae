662596c073e96b77936f4677cef9859a48c2efa4
Herbert Lee <herbert.lee@striiv.com>
2019-10-06 22:42:17 -0700

    Added app engine deployment to ci/cd pipeline.

--- deployments/app_engine/deploy.sh
+++ deployments/app_engine/deploy.sh
@@ -5,7 +5,7 @@ mkdir target
 
 j2 app.yaml.jinja2 > target/app.yaml
 cp main.py target
-cp requirements.txt target
-cp -r  target
+cp ../../requirements.txt target
+cp -r ../../mdwiki_gae target
 
 gcloud app deploy target/app.yaml --project $GOOGLE_CLOUD_PROJECT --quiet
