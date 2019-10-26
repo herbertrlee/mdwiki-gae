1b3549b1e52b2090698f065ad4526985f3b4f01c
Herbert Lee <herbert.lee@striiv.com>
2019-10-26 15:56:42 -0700

    Index title of pages.  Use bulk API instead of individual API calls.

--- scripts/index_documents.py
+++ scripts/index_documents.py
@@ -1,5 +1,7 @@
+import base64
 import os
 
+import elasticsearch.helpers
 from elasticsearch import Elasticsearch
 from google.cloud import storage
 
@@ -14,11 +16,17 @@ elasticsearch_client.indices.create(index='pages', ignore=400)
 storage_client = storage.Client()
 bucket = storage_client.bucket(GCS_BUCKET)
 
-for blob in bucket.list_blobs():
+def create_action(blob: storage.Blob):
     blob_id = blob.name
+    encoded_id = base64.b64encode(blob_id.encode('utf-8')).decode('utf-8')
+    blob_contents = blob.download_as_string().decode('utf-8')
+    metadata = blob.metadata or {}
+    title = metadata.get("title", "Untitled")
+    return {"text": blob_contents, "title": title, "_id": encoded_id}
 
-    if blob_id.endswith(".md"):
-        blob_contents = blob.download_as_string().decode('utf-8')
+def should_process(blob: storage.Blob):
+    return blob.name.endswith(".md")
 
-        document = {"text": blob_contents, "name": blob_id}
-        elasticsearch_client.index(index='pages', body=document)
+actions = iter(create_action(blob) for blob in bucket.list_blobs() if should_process(blob))
+
+elasticsearch.helpers.bulk(elasticsearch_client, actions, index="pages")
